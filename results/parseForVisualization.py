befd0ad8dc00e2c3202788fbdb68769bfccfdf29
Raghav Gupta <rgupta1@br011.ib.bridges2.psc.edu>
2022-12-08 23:35:40 -0500

    Results for bridges machine testing

--- results/parseForVisualization.py
+++ results/parseForVisualization.py
@@ -7,16 +7,29 @@ b = []
 for line in f.readlines():
   s = line.split()
   if s[0] == 'Parallel':
-    # a.append(int(s[2]))
-    print(s[2])
+    a.append(int(s[2]))
+    # print(s[2])
   if s[0] == 'Time' and s[1] == 'difference':
     p = s[3].split('[')
     b.append(int(p[0]))
 
+res = []
 
-for i in range(len(b)):
-  # print(str(a[(int)(i/3) ]) + '\t' + str(b[i]))
-  print(str(b[i]))
+n = len(b)
+i = 0
+while i < n:
+    avg = (b[i] + b[i + 1] + b[i + 2])/3
+    res.append((a[(int)(i/3)], avg) )
+    i += 3
+  
+print(res)
+
+#for threadCount in resDict:
+#  resDict[threadCount] /= (len(b) / len(a))
+
+
+
+
 
 # print(*a, sep='\n')
 # print(*b, sep='\n')
